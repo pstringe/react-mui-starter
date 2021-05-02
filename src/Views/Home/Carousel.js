52cafa1ab9706f6fd6668818a071726110a28bab
Poitier Stringer <stringerpoitier@gmail.com>
2021-05-02 12:09:31 -0700

    constant height carasoul

--- src/Views/Home/Carousel.js
+++ src/Views/Home/Carousel.js
@@ -17,6 +17,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
+    height: 350,
   },
   overlay: {
     position: 'absolute',
